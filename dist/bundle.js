96dca7dbf0a8e5e7a70a39064825445ee5609a5a
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-05-07 10:37:34 -0400

    Fix transparent player issue

--- dist/bundle.js
+++ dist/bundle.js
@@ -78,25 +78,50 @@ class Tile {
     this.imageSrc = "";
   }
 
+  createBitmap(stage, imagePath, x, y) {
+    const bitmap = new createjs.Bitmap(imagePath);
+          bitmap.scaleX = 0.5;
+          bitmap.scaleY = 0.5;
+          bitmap.x = this.column * 64;
+          bitmap.y = this.row * 64;
+
+    stage.addChild(bitmap);
+    stage.update();
+  }
+
   render(stage) {
     let image = new Image();
-    if (this.player) {
-      image.src = this.player.imageSrc;
-    } else if (this.box) {
-      image.src = this.box.imageSrc;
-    } else {
-      image.src = this.imageSrc;
-    }
 
-    image.onload = (event) => {
-      const bitmap = new createjs.Bitmap(event.target);
-      bitmap.scaleX = 0.5;
-      bitmap.scaleY = 0.5;
-      bitmap.x = this.column * 64;
-      bitmap.y = this.row * 64;
-      stage.addChild(bitmap);
-      stage.update();
-    };
+    // One object on the grid will have a valid player property
+    // Load the background image first, then the player on top
+    if (this.player) {
+
+      image.src = this.player.imageSrc;
+      const backgroundImage = new Image();
+            backgroundImage.src = this.imageSrc;
+
+      backgroundImage.onload = (event) => {
+        this.createBitmap(stage, event.target, this.column * 64, this.row * 64);
+
+        image.onload = (playerEvent) => {
+          this.createBitmap(stage, playerEvent.target, this.column * 64, this.row * 64);
+        };
+      };
+
+    } else if (this.box) {
+
+      image.src = this.box.imageSrc;
+      image.onload = (event) => {
+        this.createBitmap(stage, event.target, this.column * 64, this.row * 64);
+      };
+
+    } else {
+
+      image.src = this.imageSrc;
+      image.onload = (event) => {
+        this.createBitmap(stage, event.target, this.column * 64, this.row * 64);
+      };
+    }
   }
 
 }
